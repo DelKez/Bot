e1bc899d49ceed25a813c9e5d33dbf547d995377
DelKez <47643868+DelKez@users.noreply.github.com>
2021-04-15 10:59:37 +0200

    Update evenement.js

--- tool/evenement.js
+++ tool/evenement.js
@@ -1,7 +1,7 @@
 // APPEL FICHIERS
 const Fortnite = require("../modules/stats/fortnite")
 
-module.exports.cmd = function (type,client,""){
+module.exports.cmd = function (type,client," "){
 
     if (type == "fortnite"){
         Fortnite.cmd(["!ft","tlm"],client)
