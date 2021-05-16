2f2cf5f2d6b31f1752ca546ccbf829fe4c46e20b
DelKez <47643868+DelKez@users.noreply.github.com>
2021-05-16 13:40:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,11 +27,6 @@ var mot = "...";
 client.on('message', function (message) { 
     cmd = message.content
     
-    if (message.member.id == "422388406346055681") {
-        message.channel.send("Plus qu'un message avant le ban définitif");
-        message.delete();
-    }
-    
     if (cmd[0] == "!") {
         
         ID = message.member.id
